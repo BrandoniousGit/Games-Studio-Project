380d0d8db086c45ae486cd9af28173e61598d5f0
BrandoniousGit <72399815+BrandoniousGit@users.noreply.github.com>
2022-03-01 16:23:25 +0000

    Wallace
    Added skybox

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -10,9 +11,11 @@ public class PlayerMove : MonoBehaviour
     private Transform playerTrans;
 
     public Transform playerCenter;
+    public GameObject arrow;
     public Vector3 CameraOffset;
+    public GameObject projectile;
 
-    public float moveSpeed, jumpForce, timer;
+    public float moveSpeed, jumpForce, counter;
 
     public bool isGrounded, alive = true;
 
@@ -27,27 +30,43 @@ public class PlayerMove : MonoBehaviour
     {
         cam.transform.position = playerCenter.position + CameraOffset;
 
+        Jump();
+        ClampVelocity();
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GameObject clone;
+            clone = Instantiate(projectile, playerTrans.position, arrow.transform.rotation);
+
+            clone.GetComponent<Rigidbody>().velocity = arrow.transform.TransformDirection(Vector3.up * 5);
+            StartCoroutine(removeBullet(clone));
+        }
+    }
+
+    public void Jump()
+    {
         RaycastHit hit;
-        if (Physics.SphereCast(playerCenter.position, (playerTrans.localScale.x / 2) - 0.05f, -Vector3.up, out hit, 0.1f))
+        if (Physics.SphereCast(playerCenter.position, (playerTrans.localScale.x / 2) - 0.05f, -Vector3.up, out hit, 0.1f) && (Input.GetAxis("Jump") == 0))
         {
             isGrounded = true;
             jumpForce = 5;
         }
-
-        ClampVelocity();
+        else if (Input.GetAxis("Jump") == 0)
+        {
+            jumpForce = 0;
+        }
 
         if (Input.GetAxis("Jump") == 1)
         {
             isGrounded = false;
-            jumpForce -= 0.06f;
             if (jumpForce > 1.5f)
             {
                 playerRB.velocity = new Vector3(playerRB.velocity.x, jumpForce, 0);
             }
-        }
-        else if(Input.GetAxis("Jump") == 0)
-        {
-            jumpForce = 0;
         }
     }
 
@@ -61,6 +80,10 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (Input.GetAxis("Jump") == 1)
+        {
+            jumpForce -= 0.15f;
+        }
         if (alive == true)
         {
             YourInput();
@@ -81,5 +104,11 @@ public class PlayerMove : MonoBehaviour
         }
 
         else { playerRB.velocity = new Vector3(0, playerRB.velocity.y, 0); }
+    }
+
+    IEnumerator removeBullet(GameObject bullet)
+    {
+        yield return new WaitForSeconds(100);
+        Destroy(bullet);
     }
 }
