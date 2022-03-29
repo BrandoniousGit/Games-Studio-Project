e339935c6443645c56083cc373189f40270f23cc
BrandoniousGit <72399815+BrandoniousGit@users.noreply.github.com>
2022-03-29 15:07:49 +0100

    Crosshair Fix
    Fixed crosshair jittering

--- Assets/Scripts/WhereMouse.cs
+++ Assets/Scripts/WhereMouse.cs
@@ -21,7 +21,7 @@ public class WhereMouse : MonoBehaviour
         Vector3 relative = transform.InverseTransformPoint(cursor.position);
         float angle = Mathf.Atan2(relative.x, relative.y);
 
-        Debug.DrawLine(arrow.position, shootThisWay.position, Color.yellow);
+        //Debug.DrawLine(arrow.position, shootThisWay.position, Color.yellow);
 
         cursor.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.3f));
         arrow.transform.eulerAngles = new Vector3(0, 0, (-angle * Mathf.Rad2Deg));
