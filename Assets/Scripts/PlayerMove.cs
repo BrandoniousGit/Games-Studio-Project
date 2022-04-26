448f52c4565b23028ba83dc0e01fea293170ce8b
BrandoniousGit <72399815+BrandoniousGit@users.noreply.github.com>
2022-04-26 13:53:54 +0100

    Movement
    Made the movement good

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -11,7 +12,8 @@ public class PlayerMove : MonoBehaviour
 
     //Public Objects
     public Transform playerCenter;
-    public Vector3 CameraOffset;
+    public Vector3 cameraOffset;
+    public Image healthBar;
 
     //Public Variables
     public float moveSpeed, jumpForce, timeInAir, maxJumpTime, initialJump;
@@ -25,10 +27,28 @@ public class PlayerMove : MonoBehaviour
         playerTrans = GetComponent<Transform>();
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+            if (other.gameObject.tag == "BananaHealthSmall")
+            {
+                Destroy(other.gameObject);
+                healthBar.fillAmount += 0.25f;
+            }
+            if (other.gameObject.tag == "BananaHealthBig")
+            {
+                Destroy(other.gameObject);
+                healthBar.fillAmount += 0.40f;
+            }
+        if (other.gameObject.tag == "DeathPlane")
+        {
+
+        }
+    }
+
     void Update()
     {
         //Camera offset
-        cam.transform.position = playerCenter.position + CameraOffset;
+        //cam.transform.position = playerCenter.position + cameraOffset;
 
         RaycastHit hit;
         //Boxcast below the player to check for ground
@@ -45,7 +65,7 @@ public class PlayerMove : MonoBehaviour
         //Jump Script
         if (Input.GetButton("Jump") && canJump == true && GetComponent<Grapple>().isGrappling == false)
         {
-            if (timeInAir < maxJumpTime)
+            /*if (timeInAir < maxJumpTime)
             {
                 //At the beginning of the jump, set the velocity to make the jump more realistic
                 if (timeInAir == 0)
@@ -54,8 +74,17 @@ public class PlayerMove : MonoBehaviour
                 }
                 playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 timeInAir += Time.deltaTime;
-            }
+            }*/
             //If the player lets go of space the jump stops
+            if (timeInAir < maxJumpTime)
+            {
+                if (timeInAir == 0)
+                {
+                    playerRB.AddForce(Vector3.up * initialJump, ForceMode.Force);
+                }
+                playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Force);
+                timeInAir += Time.deltaTime;
+            }
             else
             {
                 canJump = false;
@@ -64,15 +93,6 @@ public class PlayerMove : MonoBehaviour
         else if (Input.GetButtonUp("Jump"))
         {
             canJump = false;
-        }
-    }
-
-    //Unnecessary code
-    public void ClampVelocity()
-    {
-        if (playerRB.velocity.y >= 0.5f)
-        {
-            playerRB.velocity = new Vector3(playerRB.velocity.x, 0.5f, 0);
         }
     }
 
@@ -98,9 +118,9 @@ public class PlayerMove : MonoBehaviour
             playerRB.velocity = new Vector3(moveSpeed, playerRB.velocity.y, 0);
         }
 
-        else
+        else if (GetComponent<Grapple>().isGrappling == false)
         {
-            playerRB.velocity = new Vector3(playerRB.velocity.x / 1.08f, playerRB.velocity.y, 0);
+            playerRB.velocity = new Vector3(playerRB.velocity.x / 1.07f, playerRB.velocity.y, 0);
         }
     }
 }
